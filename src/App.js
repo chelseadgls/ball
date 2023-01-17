e2cab425db89d67721404dc78dc356c13c9f3471
chelseadgls <chelseamdouglas@gmail.com>
2023-01-17 18:48:12 -0500

    players loading

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
 function App() {
+  const [players, setPlayers] = useState([]);
+
+  const getPlayers = async () => {
+    try {
+      const response = await axios.get('https://www.balldontlie.io/api/v1/players');
+      console.log(response.data.data)
+      setPlayers(response.data.data);
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  useEffect(() => {
+    try {
+      getPlayers();
+    } catch (error) {
+      throw error;
+    }
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Players</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>dummy</th>
+            <th>dummy</th>
+            <th>dummy</th>
+          </tr>
+        </thead>
+        <tbody>
+          {
+            Array.from(players).map((player, key) => 
+              <tr key={key}>
+                <td className='table-data'>{player.last_name}</td>
+                <td className='table-data'>{player.first_name}</td>
+                <td className='table-data'>{player.weight_pounds}</td>
+              </tr>
+            )
+          }
+        </tbody>
+      </table>
     </div>
   );
 }
